d8685bd52521c317f13183d469f338ff8d5fbcd9
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-09 15:48:33 +0200

    basic usage of rust driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 #![deny(clippy::all)]
 
+use scylla::SessionBuilder;
+
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub async fn test_connection(uri: String) -> String {
+
+  println!("Connecting to {} ...", uri);
+
+  match SessionBuilder::new().known_node(uri).build().await {
+    Ok(data) => data,
+    Err(_) => {
+      return "ErrorXD".to_string();
+    }
+  };
+  "Success".to_string()
 }
