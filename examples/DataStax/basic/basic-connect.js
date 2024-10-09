619813e3a0e4df7708d085c03297c4d061e50218
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-09 15:53:43 +0200

    Grab tests and examples from DataStax driver

---
+++ examples/DataStax/basic/basic-connect.js
@@ -0,0 +1,15 @@
+"use strict";
+const cassandra = require('scylladb-javascript-driver');
+
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'dc1' });
+client.connect()
+  .then(function () {
+    console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+    console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+    console.log('Shutting down');
+    return client.shutdown();
+  })
+  .catch(function (err) {
+    console.error('There was an error when connecting', err);
+    return client.shutdown().then(() => { throw err; });
+  });
---
+++ examples/DataStax/basic/basic-execute.js
@@ -0,0 +1,22 @@
+"use strict";
+const cassandra = require('scylladb-javascript-driver');
+
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'dc1' });
+
+/**
+ * Example using Promise.
+ * See basic-execute-flow.js for an example using callback-based execution.
+ */
+client.connect()
+  .then(function () {
+    return client.execute('SELECT * FROM system.local');
+  })
+  .then(function (result) {
+    const row = result.rows[0];
+    console.log('Obtained row: ', row);
+  })
+  .finally(() => client.shutdown());
+
+
+// Exit on unhandledRejection
+process.on('unhandledRejection', (reason) => { throw reason; });
