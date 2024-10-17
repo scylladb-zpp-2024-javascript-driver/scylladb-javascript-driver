674d3b3c0298c6fea703ee2a2af278eb0d2ab6a7
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-17 22:20:16 +0200

    Add implementation for CqlDuration
    Creates custom struct/class DurationWrapper
    that allows passing CqlDuration between rust and JS parts of the driver
    
    Cql duration wrapper is kept as an internal object in JS duration class.

--- src/types/duration.rs
+++ src/types/duration.rs
@@ -1,34 +1,23 @@
 use napi::bindgen_prelude::BigInt;
+use scylla::frame::value::CqlDuration;
 
-use crate::utils::js_error;
+use crate::utils::bigint_to_i64;
 
 #[napi]
-// Will PartialEq or Eq be exposed to napi?
-#[derive(Clone, Debug, Copy, PartialEq, Eq)]
-pub struct Duration {
+pub struct DurationWrapper {
     pub months: i32,
     pub days: i32,
     pub nanoseconds: i64,
 }
 
 #[napi]
-impl Duration {
+impl DurationWrapper {
     #[napi]
     pub fn new(months: i32, days: i32, ns_bigint: BigInt) -> napi::Result<Self> {
-        let ns = ns_bigint.get_i64();
-        if !ns.1 {
-            return Err(js_error("Invalid use: Should not happen?"));
-        }
-        let nanoseconds: i64 = ns.0
-            * if ns_bigint.sign_bit && ns.0 > 0 {
-                -1
-            } else {
-                1
-            };
-        Ok(Duration {
+        Ok(DurationWrapper {
             months,
             days,
-            nanoseconds,
+            nanoseconds: bigint_to_i64(ns_bigint, "Nanoseconds must not overflow i64")?,
         })
     }
 
@@ -38,5 +27,22 @@ impl Duration {
         let mut res: BigInt = BigInt::from(tmp.abs());
         res.sign_bit = self.nanoseconds < 0;
         res
+    }
+}
+
+impl DurationWrapper {
+    pub fn from_cql_duration(duration: CqlDuration) -> Self {
+        DurationWrapper {
+            months: duration.months,
+            days: duration.days,
+            nanoseconds: duration.nanoseconds,
+        }
+    }
+    pub fn get_cql_duration(&self) -> CqlDuration {
+        CqlDuration {
+            months: self.months,
+            days: self.days,
+            nanoseconds: self.nanoseconds,
+        }
     }
 }
