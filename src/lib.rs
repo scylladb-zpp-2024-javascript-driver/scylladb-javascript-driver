2a1d6f093b4b38b7b4c19c0d1517846961a7eb96
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-17 09:24:42 +0200

    Rust duration type This is a example implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 use scylla::SessionBuilder;
 
 #[macro_use]
@@ -8,7 +9,7 @@ extern crate napi_derive;
 #[napi]
 pub async fn test_connection(uri: String) -> String {
   println!("Connecting to {} ...", uri);
-
+  
   match SessionBuilder::new().known_node(uri).build().await {
     Ok(data) => data,
     Err(_) => {
@@ -20,5 +21,6 @@ pub async fn test_connection(uri: String) -> String {
 
 // Link other file
 pub mod auth;
+pub mod types;
 pub mod result;
 pub mod session;
