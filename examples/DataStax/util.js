daacfa2bc3859180d46a524a294ccfa5772b3b43
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-18 17:47:44 +0200

    Add port

--- examples/DataStax/util.js
+++ examples/DataStax/util.js
@@ -1,6 +1,6 @@
 function getClientArgs() {
     return {
-        contactPoints: [process.env.SCYLLA_URI ?? '172.17.0.2'],
+        contactPoints: [process.env.SCYLLA_URI ?? '172.17.0.2:9042'],
         localDataCenter: process.env.DATACENTER ?? 'datacenter1'
     };
 }
