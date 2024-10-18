339bc17d6eb17aa4fad286b4d08e2f38809d3ec6
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-18 16:35:03 +0200

    Update examples readme

--- examples/DataStax/util.js
+++ examples/DataStax/util.js
@@ -1,7 +1,7 @@
 function getClientArgs() {
     return {
         contactPoints: [process.env.SCYLLA_URI?.split(':')[0] ?? '172.17.0.2'],
-        localDataCenter: process.env.DATACENTER?.split(':')[0] ?? 'datacenter1'
+        localDataCenter: process.env.DATACENTER ?? 'datacenter1'
     };
 }
 
