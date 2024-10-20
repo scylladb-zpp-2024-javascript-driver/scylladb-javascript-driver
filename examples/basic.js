edbad7c39db0272280ad13efed9b87d40ceda3b7
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-20 17:56:22 +0200

    cleanup

--- examples/basic.js
+++ examples/basic.js
@@ -1,7 +1,7 @@
-const lib = require("../");
+const lib = require("../index");
 
 async function fn() {
-  console.log(await lib.rust.testConnection("172.17.0.2:9042"));
+  console.log(await lib.testConnection("172.17.0.2:9042"));
 }
 
 fn();
