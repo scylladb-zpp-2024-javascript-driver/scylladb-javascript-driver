812083843a869f7d0872fbd8241205c3efbba028
Zuzanna Ossowska <zuza.os@wp.pl>
2024-10-24 08:33:48 +0200

    Add error message test

--- test/unit/graph/graph-result-tests.js
+++ test/unit/graph/graph-result-tests.js
@@ -9,4 +9,9 @@ describe("GraphResultSet", function () {
       new GraphResultSet();
     }, ReferenceError);
   });
+  it("tests the not supported error message", function() {
+    assert.throws(function () {
+      new GraphResultSet();
+    }, {name: 'ReferenceError', message: 'The class (GraphResultSet) is not supported by our driver'})
+  });
 });
