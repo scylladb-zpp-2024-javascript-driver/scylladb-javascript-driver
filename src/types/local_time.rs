4389ed6d357d6b322f043f6ecc0a5681e6def6a9
Stapox35 <stapox35@gmail.com>
2024-11-12 22:55:59 +0100

    Implement LocalTime.fromString() function in Rust.
    Move the function logic to rust.
    Add date format regex checking in JS.

--- src/types/local_time.rs
+++ src/types/local_time.rs
@@ -1,10 +1,14 @@
+use crate::utils::{bigint_to_i64, js_error};
 use core::fmt;
 use napi::bindgen_prelude::BigInt;
 use scylla::frame::value::CqlTime;
-
-use crate::utils::bigint_to_i64;
+use std::num::ParseIntError;
 
 const NANO_SEC_IN_SEC: i64 = 1000000000;
+const MILLIS_SEC_IN_HOUR: i64 = 3_600_000;
+const MILLIS_SEC_IN_MIN: i64 = 60_000;
+const MILLIS_SEC_IN_SEC: i64 = 1000;
+const NANO_SEC_IN_MILLIS: i64 = 1_000_000;
 
 #[napi]
 pub struct LocalTimeWrapper {
@@ -45,10 +49,36 @@ impl LocalTimeWrapper {
         Ok(Self::convert_to_object(ns_value))
     }
 
-    //format: hh:MM:ss.ns
+    /// format: hh:MM:ss.ns
     #[napi(js_name = "toString")]
     pub fn to_format(&self) -> String {
         self.to_string()
+    }
+
+    #[napi]
+    pub fn from_string(s: String) -> napi::Result<BigInt> {
+        let lambda = |s: String| -> Result<i64, ParseIntError> {
+            let parts: Vec<&str> = s.split(':').collect();
+            let mut millis: i64 = 0;
+
+            millis += parts[0].parse::<i64>()? * MILLIS_SEC_IN_HOUR;
+            millis += parts[1].parse::<i64>()? * MILLIS_SEC_IN_MIN;
+            let mut nanos = 0;
+            if parts.len() == 3 {
+                let sec_parts: Vec<&str> = parts[2].split('.').collect();
+                millis += sec_parts[0].parse::<i64>()? * MILLIS_SEC_IN_SEC;
+                if sec_parts.len() == 2 {
+                    let n = 9 - sec_parts[1].to_string().chars().count() as u32;
+                    nanos += sec_parts[1].parse::<i64>()? * i64::pow(10, n);
+                }
+            }
+            Ok(millis * NANO_SEC_IN_MILLIS + nanos)
+        };
+
+        match lambda(s) {
+            Ok(x) => Ok(BigInt::from(x)),
+            Err(_) => Err(js_error("Conversion to String failed")),
+        }
     }
 
     pub fn get_cql_time(&self) -> CqlTime {
