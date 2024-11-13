5c1bc7bb6c1461d3ba1677d0de4950aabed88724
Stanisław Czech <sc@lo3gdynia.pl>
2024-11-13 19:52:05 +0100

    BigInt converter function
    Adds function bigint_to_i64, as napi logic currently has a bug
    Adds unit tests for this function

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, fmt::Display};
 
-use napi::Status;
+use napi::{bindgen_prelude::BigInt, Status};
 
 /// Convert rust error to napi::Error
 pub(crate) fn err_to_napi<T: Error>(e: T) -> napi::Error {
@@ -10,4 +10,16 @@ pub(crate) fn err_to_napi<T: Error>(e: T) -> napi::Error {
 /// Create napi::Error from a message
 pub(crate) fn js_error<T: Display>(e: T) -> napi::Error {
     napi::Error::new(Status::GenericFailure, e.to_string())
+}
+
+/// Convert bigint to i64. Returns napi::Error if value doesn't fit in i64.
+pub(crate) fn bigint_to_i64(value: BigInt, error_msg: impl Display) -> napi::Result<i64> {
+    // Currently BigInt.get_i64() doesn't work as intended, so for now convert it manually
+    if value.words.len() != 1 || value.words[0] > i64::MAX as u64 {
+        if value.sign_bit && value.words.first().unwrap_or(&0) == &i64::MIN.unsigned_abs() {
+            return Ok(i64::MIN);
+        }
+        return Err(js_error(error_msg));
+    }
+    Ok(value.words[0] as i64 * if value.sign_bit { -1 } else { 1 })
 }
