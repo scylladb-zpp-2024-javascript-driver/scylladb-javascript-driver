183e2f386b4e26eff956ededf444c2702fa6f338
ZuzaOsa <zuza.os@wp.pl>
2024-11-15 20:33:29 +0100

    Add eslint config
    Rule about using prototypes was switched off, Datastax code violates it
    After switching the code to use classes the rule should be switched on
    Added camelcase, no-var and spaced-comment rules
    no-unused-vars rule was switched off for catches and function parameters

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  {
+    ignores: ["test/integration/not-supported/**"],
+  }, 
+  {
+    files: ["lib/**/*.js", "test/**/*.js", "examples/**/*.js"], 
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.mocha,
+        ...globals.node,
+        expect: "readonly",
+        assert: "readonly",
+      },
+    },
+    rules: {
+      // Check if there is no unused variables
+      // If a value is passed to a function but not used the error is ignored
+      // Catched but unused errors are also ignored
+      "no-unused-vars": ["error", { "args": "none", "caughtErrors": "none" }],
+
+      //Checks if var is not used
+      "no-var": "error",
+
+      //Check if there is a space at the beginning of a comment
+      "spaced-comment": "error",
+
+      // This will be deleted after everything is switched from prototypes to classes
+      "no-prototype-builtins": "off",
+
+      // Check if variable names are in camelCase
+      // Disabled in some of the tests because they are inserting values into the database
+      "camelcase": "error",
+
+      "no-constructor-return": "error",
+      "no-duplicate-imports": "error",
+      "default-case-last": "error",
+      "no-eval": "error",
+      "no-multi-str": "error",
+      "no-throw-literal": "error",
+      "sort-imports": "error",
+      "no-lonely-if": "error",
+      "no-else-return": "error",
+      "no-useless-assignment": "error",
+    }
+  },
+];
