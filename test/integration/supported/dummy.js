620b22ad5111aa7f39c6aa9e24f384fadbbc888d
Stanisław Czech <sc@lo3gdynia.pl>
2024-11-15 19:36:39 +0100

    Enable working integration tests and examples
    For enabled integration tests enable only some specific parts,
    as not all features to fully pass them are implemented.

--- test/integration/supported/dummy.js
+++
@@ -1,5 +0,0 @@
-// Dummy test just for code structure purpose.
-// Safe to remove when other tests will be present in this directory
-describe("dummy", function () {
-    console.log(`Some dummy integration test.`);
-});
