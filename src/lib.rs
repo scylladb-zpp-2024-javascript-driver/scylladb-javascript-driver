7c16ff34faf86944f3f32d43c75980ff884c1edf
Stanisław Czech <sc@lo3gdynia.pl>
2024-11-20 13:22:39 +0100

    Add driver Self Identity
    Take application name and version from user application
    and use driver name defined in rust options file

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ pub async fn test_connection(uri: String) -> String {
 
 // Link other file
 pub mod auth;
+pub mod options;
 pub mod result;
 pub mod session;
 pub mod tests;
--- src/session.rs
+++ src/session.rs
@@ -1,10 +1,16 @@
-use scylla::{Session, SessionBuilder};
+use scylla::{transport::SelfIdentity, Session, SessionBuilder};
 
-use crate::{result::QueryResultWrapper, utils::err_to_napi};
+use crate::{
+    options::{self},
+    result::QueryResultWrapper,
+    utils::err_to_napi,
+};
 
 #[napi]
 pub struct SessionOptions {
     pub connect_points: Vec<String>,
+    pub application_name: Option<String>,
+    pub application_version: Option<String>,
 }
 
 #[napi]
@@ -18,6 +24,8 @@ impl SessionOptions {
     pub fn empty() -> Self {
         SessionOptions {
             connect_points: vec![],
+            application_name: None,
+            application_version: None,
         }
     }
 }
@@ -28,6 +36,7 @@ impl SessionWrapper {
     pub async fn create_session(options: &SessionOptions) -> napi::Result<Self> {
         let s = SessionBuilder::new()
             .known_node(options.connect_points[0].clone())
+            .custom_identity(get_self_identity(options))
             .build()
             .await
             .map_err(err_to_napi)?;
@@ -43,4 +52,17 @@ impl SessionWrapper {
             .map_err(err_to_napi)?;
         Ok(QueryResultWrapper::from_query(query_result))
     }
+}
+
+fn get_self_identity(options: &SessionOptions) -> SelfIdentity<'static> {
+    let mut self_identity = SelfIdentity::new();
+    self_identity.set_custom_driver_name(options::DEFAULT_DRIVER_NAME);
+    self_identity.set_application_version(options::DEFAULT_DRIVER_VERSION);
+    if let Some(app_name) = &options.application_name {
+        self_identity.set_application_name(app_name.clone());
+    }
+    if let Some(app_version) = &options.application_name {
+        self_identity.set_application_version(app_version.clone());
+    }
+    self_identity
 }
