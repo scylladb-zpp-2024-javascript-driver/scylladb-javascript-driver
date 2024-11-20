4d06f00412d5dcb531db53fb3e62b76a9262ff8b
Stanisław Czech <sc@lo3gdynia.pl>
2024-11-20 22:38:01 +0100

    Some code for simple benchmarks
    Co-authored-by:  ZuzaOsa <zuza.os@wp.pl>

---
+++ benchmark/logic_rust/insert.rs
@@ -0,0 +1,42 @@
+use scylla::client::session_builder::SessionBuilder;
+use std::env;
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let n: i32 = env::var("CNT")
+        .ok()
+        .and_then(|s| s.parse::<i32>().ok())
+        .unwrap_or(100);
+    let session = SessionBuilder::new()
+        .known_node("172.17.0.2:9042")
+        .build()
+        .await?;
+
+    session
+        .query_unpaged(
+            "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1' }", 
+            &[],
+        )
+        .await?;
+
+    session
+        .query_unpaged("DROP TABLE IF EXISTS benchmarks.basic", &[])
+        .await?;
+
+    session
+        .query_unpaged(
+            "CREATE TABLE benchmarks.basic (id uuid, val int, PRIMARY KEY(id))",
+            &[],
+        )
+        .await?;
+
+    let insert_query = "INSERT INTO benchmarks.basic (id, val) VALUES (?, ?)";
+    for _ in 0..n {
+        let id = Uuid::new_v4();
+        session.query_unpaged(insert_query, (id, 100)).await?;
+    }
+
+    // println!("Completed");
+    Ok(())
+}
