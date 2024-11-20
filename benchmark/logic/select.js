5a7f8784e96538d33869b778f3f41fae4b9e6343
Stanisław Czech <sc@lo3gdynia.pl>
2024-11-20 22:38:01 +0100

    Some code for simple benchmarks
    Co-authored-by:  ZuzaOsa <zuza.os@wp.pl>

---
+++ benchmark/logic/select.js
@@ -0,0 +1,67 @@
+"use strict";
+const async = require("async");
+// Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
+const cassandra = require(process.argv[2]);
+const { getClientArgs } = require("./utils");
+const { exit } = require("process");
+
+const client = new cassandra.Client(getClientArgs());
+const iterCnt = parseInt(process.argv[3]);
+
+async.series(
+    [
+        function connect(next) {
+            client.connect(next);
+        },
+        function createKeyspace(next) {
+            // Keep replication one to reduce time spent in the database
+            const query =
+                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
+            client.execute(query, next);
+        },
+        // Drop table to ensure no overhead comes from having already some data in the database
+        function dropTable(next) {
+            const query =
+                "DROP TABLE IF EXISTS benchmarks.basic";
+            client.execute(query, next);
+        },
+        function createTable(next) {
+            const query =
+                "CREATE TABLE benchmarks.basic (id uuid, val int, PRIMARY KEY(id))";
+            client.execute(query, next);
+        },
+        async function insert(next) {
+            let query =
+                "INSERT INTO benchmarks.basic (id, val) VALUES (?, ?)";
+            for (let i = 0; i < 10; i++) {
+                let id = cassandra.types.Uuid.random();
+                try {
+                    await client.execute(query, [id, 100], {prepare: true});
+                } catch(err) {
+                    return next(err);
+                }
+            }
+            next();
+        },
+        async function query(next) {
+            const query = "SELECT * FROM benchmarks.basic";
+            for (let i = 0; i < iterCnt; i++) {
+                try {
+                    await client.execute(query);
+                } catch (err) {
+                    return next(err);
+                }
+            }
+            next();
+        },
+        function r() {
+            exit(0);
+        }
+
+    ], function (err) {
+        if (err) {
+            console.error("Error: ", err.message, err.stack);
+            exit(1);
+        }
+    },);
+
