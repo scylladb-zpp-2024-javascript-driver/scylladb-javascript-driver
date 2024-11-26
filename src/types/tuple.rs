6cd529e4090f8f160b0e10a90519ab3d5dfb1054
Piotr Maksymiuk <p.maksymiuk2@student.uw.edu.pl>
2024-11-26 18:12:22 +0100

    JS and Rust implementation of Tuple type
    Idea behind this approach it to store tuple as a list of values in JS
    when it is created by JS side of driver and as a list of CQL values in
    Rust when it is created by Rust side of driver. This was done because
    type of tuple is not known in advance and it is not possible to create
    CQL values in Rust without knowing the type.

---
+++ src/types/tuple.rs
@@ -0,0 +1,54 @@
+use crate::{result::CqlValueWrapper, utils::js_error};
+use scylla::value::CqlValue;
+
+#[napi]
+pub struct TupleWrapper {
+    // If values == None, then Tuple is stored in JS part,
+    // as it is not possible to convert it to Rust without knowing expected types
+    values: Option<Vec<Option<CqlValue>>>,
+}
+
+#[napi]
+impl TupleWrapper {
+    #[napi]
+    pub fn new() -> napi::Result<Self> {
+        Ok(TupleWrapper { values: None })
+    }
+
+    #[napi]
+    pub fn get_length(&self) -> u32 {
+        self.values.as_ref().map_or(0, |v| v.len() as u32)
+    }
+
+    #[napi]
+    pub fn get(&self, index: u32) -> napi::Result<Option<CqlValueWrapper>> {
+        self.values
+            .as_ref()
+            .ok_or_else(|| js_error("Values are stored in JS"))
+            .and_then(|values| {
+                values
+                    .get(index as usize)
+                    .map(|v| v.as_ref().map(|v| CqlValueWrapper { inner: v.clone() }))
+                    .ok_or_else(|| js_error("Index out of bounds"))
+            })
+    }
+
+    #[napi]
+    pub fn get_all(&self) -> napi::Result<Vec<Option<CqlValueWrapper>>> {
+        self.values
+            .as_ref()
+            .ok_or_else(|| js_error("Values are stored in JS"))
+            .map(|values| {
+                values
+                    .iter()
+                    .map(|v| v.as_ref().map(|v| CqlValueWrapper { inner: v.clone() }))
+                    .collect()
+            })
+    }
+
+    pub fn from_cql_tuple(values: Vec<Option<CqlValue>>) -> Self {
+        TupleWrapper {
+            values: Some(values),
+        }
+    }
+}
