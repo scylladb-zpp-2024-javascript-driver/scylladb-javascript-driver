49ec84741c83c8f3cb34d299a108684c823ed396
Stapox35 <stapox35@gmail.com>
2024-11-27 10:32:19 +0100

    Add implementation of LocalDate in Rust
    Add implementation of Rust object with constracor and Cql getter/setter.
    Add some custom functions for date operations.

---
+++ src/types/local_date.rs
@@ -0,0 +1,204 @@
+use crate::utils::js_error;
+use scylla::frame::value::CqlDate;
+
+// Max and min date range of the Date class in JS.
+const MAX_JS_DATE: i32 = 100_000_000;
+const MIN_JS_DATE: i32 = -MAX_JS_DATE;
+
+// Number of leap years up to 1970.
+const LEAP_YEAR_1970: i32 = 477;
+// Number of days to the beginning of each month.
+const DAY_IN_MONTH: [i32; 12] = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+// LocalDateWrapper holds two data representations - value and day, month, year.
+// The class in JS has getters for both representations.
+// When a new LocalDateWrapper instance is created, the second representation is calculated.
+#[napi]
+pub struct LocalDateWrapper {
+    pub value: i32, // wrapper for number of days from 01.01.1970
+    pub year: Option<i32>,
+    pub month: Option<i8>,
+    pub day: Option<i8>,
+    pub in_date: bool, // value can be represented as Date class in JS
+}
+
+#[napi]
+impl LocalDateWrapper {
+    /// Create a new object from the day, month and year.
+    #[napi]
+    pub fn new(day: i8, month: i8, year: i32) -> napi::Result<Self> {
+        Self::check_valid_date(Ymd { day, month, year })
+    }
+
+    /// Create a new object from number of days since 01.01.1970.
+    #[napi]
+    pub fn new_day(value: i32) -> napi::Result<Self> {
+        let date = Ymd::from_days(value.into())
+            .map(|x| (Some(x.year), Some(x.month), Some(x.day)))
+            .unwrap_or((None, None, None));
+        Ok(LocalDateWrapper {
+            value,
+            year: date.0,
+            month: date.1,
+            day: date.2,
+            in_date: (MIN_JS_DATE..=MAX_JS_DATE).contains(&value),
+        })
+    }
+
+    /// Returns the number of days in a month.
+    fn days_in_month(month: i8, year: i32) -> i8 {
+        match month {
+            1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+            4 | 6 | 9 | 11 => 30,
+            2 => {
+                if is_leap_year(year) {
+                    29
+                } else {
+                    28
+                }
+            }
+            _ => 0,
+        }
+    }
+
+    /// Checks if the given date is correct and returns Self.
+    fn check_valid_date(date: Ymd) -> napi::Result<Self> {
+        if !(1..=12).contains(&date.month) {
+            return Err(js_error("Invalid month"));
+        }
+
+        if !(date.day >= 1 && date.day <= Self::days_in_month(date.month, date.year)) {
+            return Err(js_error("Invalid number of day"));
+        }
+
+        let value = date.to_days();
+
+        Ok(LocalDateWrapper {
+            year: Some(date.year),
+            month: Some(date.month),
+            day: Some(date.day),
+            value,
+            in_date: (MIN_JS_DATE..=MAX_JS_DATE).contains(&value),
+        })
+    }
+
+    pub fn get_cql_date(&self) -> CqlDate {
+        CqlDate(((1 << 31) + self.value) as u32)
+    }
+
+    pub fn from_cql_date(date: CqlDate) -> Self {
+        let value: i32 = date.0 as i32 - (1 << 31);
+        let date = Ymd::from_days(value.into())
+            .map(|x| (Some(x.year), Some(x.month), Some(x.day)))
+            .unwrap_or((None, None, None));
+        LocalDateWrapper {
+            value,
+            year: date.0,
+            month: date.1,
+            day: date.2,
+            in_date: (MIN_JS_DATE..=MAX_JS_DATE).contains(&value),
+        }
+    }
+}
+
+/// Checks whether the year is leap year.
+fn is_leap_year(year: i32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+fn number_leap_years(n: i32) -> i32 {
+    n / 4 - n / 100 + n / 400
+}
+
+struct Ymd {
+    year: i32,
+    month: i8,
+    day: i8,
+}
+
+impl Ymd {
+    /// Counts the number of days since 01.01.1970.
+    fn to_days(&self) -> i32 {
+        let mut total_days = 0;
+
+        let number_day = DAY_IN_MONTH[self.month as usize - 1] // number of days from 1 January
+            + if is_leap_year(self.year) && self.month > 2 {
+                0
+            } else {
+                -1
+            }
+            + self.day as i32;
+
+        if self.year >= 1970 {
+            total_days += (self.year - 1970) * 365 + number_leap_years(self.year - 1)
+                - LEAP_YEAR_1970
+                + number_day;
+        } else {
+            total_days -= if is_leap_year(self.year) { 366 } else { 365 } - number_day;
+            if self.year < 0 {
+                total_days -= (1970 - self.year - 1) * 365
+                    + number_leap_years(self.year.abs() + 1)
+                    + LEAP_YEAR_1970
+                    + 1;
+            } else {
+                total_days -= (1970 - self.year - 1) * 365 + LEAP_YEAR_1970
+                    - number_leap_years(self.year + 1);
+            }
+        }
+        total_days
+    }
+
+    /// Create a Ymd from the number of days since 01.01.1970.
+    fn from_days(mut n: i64) -> Option<Self> {
+        if !(MIN_JS_DATE..=MAX_JS_DATE).contains(&(n as i32)) {
+            None
+        } else {
+            let mut year = 1970;
+            while n.abs() >= 365 {
+                // Find the number of years in n days.
+                let k = (n / 365) as i32;
+                if year > 0 && year + k < 0 {
+                    n += 1;
+                }
+                year += k;
+
+                if k > 0 {
+                    n -= (k.abs() * 365 + number_leap_years(year - 1) - number_leap_years(year - k))
+                        as i64;
+                } else {
+                    n += (k.abs() * 365 + number_leap_years(year - k) - number_leap_years(year + 1))
+                        as i64;
+                }
+            }
+
+            if n < 0 {
+                // If the remaining number of days is negative, change the year and count the complement.
+                year -= 1;
+                n += if is_leap_year(year) { 366 } else { 365 };
+            }
+
+            if is_leap_year(year) && n >= 60 {
+                n -= 1;
+            } else if is_leap_year(year) && n > 31 {
+                return Some(Ymd {
+                    year,
+                    month: 2,
+                    day: (n - 30) as i8,
+                });
+            }
+
+            let q = DAY_IN_MONTH // Find month.
+                .iter()
+                .enumerate()
+                .filter(|&(_, &x)| i64::from(x) <= n)
+                .max_by_key(|&(_, &x)| x)
+                .map(|(index, &_value)| index + 1)
+                .unwrap();
+
+            Some(Ymd {
+                year,
+                month: q as i8,
+                day: (n - DAY_IN_MONTH[q - 1] as i64) as i8 + 1,
+            })
+        }
+    }
+}
