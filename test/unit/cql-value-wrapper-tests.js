e7ccceb051ba695ba7583284c1de31ce5c6cf3f5
Piotr Maksymiuk <p.maksymiuk2@student.uw.edu.pl>
2024-12-10 16:06:06 +0100

    Tuple request
    Adds support for convertin JS Tuple type into Rust Tuple type.

--- test/unit/cql-value-wrapper-tests.js
+++ test/unit/cql-value-wrapper-tests.js
@@ -231,6 +231,25 @@ describe("Cql value wrapper", function () {
         assert.equal(value.equals(expectedUuid), true);
     });
 
+    it("should get tuple type correctly from napi", function () {
+        let element = rust.testsGetCqlWrapperTuple();
+        let type = element.getType();
+        assert.strictEqual(type, rust.CqlType.Tuple);
+        let value = getCqlObject(element);
+        assert.instanceOf(value, Tuple);
+        /* Corresponding value:
+        let element = CqlValue::Tuple(vec![
+            Some(CqlValue::Text("some text".to_owned())),
+            Some(CqlValue::Int(1)),
+            None,
+        ]); */
+        assert.strictEqual(value.length, 3);
+        console.log(value.get(0));
+        assert.strictEqual(value.get(0), "some text");
+        assert.strictEqual(value.get(1), 1);
+        assert.strictEqual(value.get(2), null);
+    });
+
     it("should get time uuid type correctly from napi", function () {
         let element = rust.testsGetCqlWrapperTimeUuid();
         let type = element.getType();
