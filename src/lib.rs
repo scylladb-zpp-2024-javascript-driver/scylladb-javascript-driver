e05e0d95aa4d1b9bbadf46670cfdc674413777ed
ZuzaOsa <zuza.os@wp.pl>
2025-01-21 16:46:20 +0100

    Remove test_connection from lib.rs
    This was a simple function used to check connection to the database
    using Rust driver.
    It was used in the examples/basic.js file, which was also removed as
    the connection is tested in basic-connect.js example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,5 @@
-use scylla::SessionBuilder;
-
 #[macro_use]
 extern crate napi_derive;
-
-// Just a basic function to test if we can run the rust driver
-// Will return "Success" only if connects to running scylla database
-#[napi]
-pub async fn test_connection(uri: String) -> String {
-    println!("Connecting to {} ...", uri);
-
-    match SessionBuilder::new().known_node(uri).build().await {
-        Ok(data) => data,
-        Err(_) => {
-            return "Error".to_string();
-        }
-    };
-    "Success".to_string()
-}
 
 // Link other file
 pub mod auth;
