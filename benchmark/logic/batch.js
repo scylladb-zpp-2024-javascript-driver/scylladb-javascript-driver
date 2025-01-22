c7a0affd041d6e5ba5f5b3836a1b99dc1663173a
Stanisław Czech <sc@lo3gdynia.pl>
2025-01-22 17:59:03 +0100

    batches

---
+++ benchmark/logic/batch.js
@@ -0,0 +1,64 @@
+"use strict";
+const async = require("async");
+const cassandra = require("scylladb-javascript-driver");
+const { getClientArgs } = require("./utils");
+const { exit } = require("process");
+const { assert } = require("console");
+const Long = require("long");
+const { randomInt } = require("crypto");
+
+const client = new cassandra.Client(getClientArgs());
+const iterCnt = 1000;
+const repet = 100;
+
+async.series(
+    [
+        function connect(next) {
+            client.connect(next);
+        },
+        function createKeyspace(next) {
+            // Keep replication one to reduce time spent in the database
+            const query =
+                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1' }";
+            client.execute(query, next);
+        },
+        // Drop tabele to ensure no overhead comes from having already some data in the database
+        function dropTable(next) {
+            const query =
+                "DROP TABLE IF EXISTS benchmarks.basic";
+            client.execute(query, next);
+        },
+        function createTable(next) {
+            const query =
+                "CREATE TABLE benchmarks.basic (id uuid, val int, PRIMARY KEY(id))";
+            client.execute(query, next);
+        },
+        async function insert(next) {
+            for (let i = 0; i < repet; i++) {
+                let allParameters = [];
+                for (let i = 0; i < iterCnt; i++) {
+                    allParameters.push({
+                        query: 'INSERT INTO benchmarks.basic (id, val) VALUES (?, ?)',
+                        params: [cassandra.types.Uuid.random(), randomInt(1 << 30 - 1)]
+                    });
+                }
+                const _result = await client.batch(allParameters);
+            }
+            next();
+        },
+        async function test(next) {
+            const query = "SELECT COUNT(1) FROM benchmarks.basic USING TIMEOUT 120s;";
+            let res = await client.execute(query);
+            console.log(res.rows[0].count)
+            assert(Long.fromInt(iterCnt * repet).equals(res.rows[0].count));
+            next();
+        },
+        function r() {
+            exit(0);
+        }
+    ]);
+
+// Exit on unhandledRejection
+process.on("unhandledRejection", (reason) => {
+    throw reason;
+});
