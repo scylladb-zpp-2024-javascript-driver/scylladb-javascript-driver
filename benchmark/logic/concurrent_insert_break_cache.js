1275e164f88184b38a9afc6723692cc7cdfeae7c
Stanisław Czech <stasio.czech@gmail.com>
2025-04-18 12:39:04 +0200

    New fancy lingering benchmark

---
+++ benchmark/logic/concurrent_insert_break_cache.js
@@ -0,0 +1,58 @@
+"use strict";
+const async = require("async");
+// Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
+const cassandra = require(process.argv[2]);
+const { getClientArgs } = require("./utils");
+const { exit } = require("process");
+const assert = require("assert");
+
+const client = new cassandra.Client(getClientArgs());
+const iterCnt = parseInt(process.argv[3]);
+
+async.series(
+    [
+        function connect(next) {
+            client.connect(next);
+        },
+        function createKeyspace(next) {
+            // Keep replication one to reduce time spent in the database
+            const query =
+                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
+            client.execute(query, next);
+        },
+        // Drop table to ensure no overhead comes from having already some data in the database
+        function dropTable(next) {
+            const query =
+                "DROP TABLE IF EXISTS benchmarks.basic2";
+            client.execute(query, next);
+        },
+        function createTable(next) {
+            const query =
+                "CREATE TABLE benchmarks.basic2 (id uuid, val text, PRIMARY KEY(id))";
+            client.execute(query, next);
+        },
+        async function insert(next) {
+            let allParameters = [];
+            for (let i = 0; i < iterCnt; i++) {
+                const myUUID = cassandra.types.Uuid.random();
+                allParameters.push({
+                    query: `INSERT INTO benchmarks.basic2 (id, val) VALUES (?, '${myUUID.toString()[0]}${myUUID.toString()[1]}')`,
+                    params: [myUUID]
+                });
+            }
+            const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, { prepare: true });
+            next();
+        },
+        async function test(next) {
+            const query = "SELECT COUNT(1) FROM benchmarks.basic2 USING TIMEOUT 120s;";
+            let res = await client.execute(query);
+            assert(res.rows[0].count == iterCnt);
+            next();
+        }
+    ],
+    function (err) {
+        if (err) {
+            console.error("There was an error", err.message, err.stack);
+            exit(1);
+        }
+    });
