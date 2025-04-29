6b98a3a25870baa06b5a59bf0e854e315e437ba2
Piotr Maksymiuk <p.maksymiuk2@student.uw.edu.pl>
2025-04-29 11:27:13 +0200

    Request test for Decimal
    Added tests to verify Decimal encoding.

--- src/tests/request_values_tests.rs
+++ src/tests/request_values_tests.rs
@@ -1,6 +1,6 @@
 use scylla::{
     cluster::metadata::{ColumnType, NativeType},
-    value::CqlTime,
+    value::{CqlDecimal, CqlTime},
 };
 use std::{
     net::{IpAddr, Ipv4Addr},
@@ -23,6 +23,7 @@ pub fn tests_from_value_get_type(test: String) -> ComplexType {
         "Boolean" => (CqlType::Boolean, None, None),
         "Blob" => (CqlType::Blob, None, None),
         "Counter" => (CqlType::Counter, None, None),
+        "Decimal" => (CqlType::Decimal, None, None),
         "Double" => (CqlType::Double, None, None),
         "Duration" => (CqlType::Duration, None, None),
         "Float" => (CqlType::Float, None, None),
@@ -65,6 +66,13 @@ pub fn tests_from_value(test: String, value: ParameterWrapper) {
         "Boolean" => CqlValue::Boolean(false),
         "Blob" => CqlValue::Blob(vec![0, 1, 2, 3]),
         "Counter" => CqlValue::Counter(Counter(921)),
+        "Decimal" => CqlValue::Decimal(CqlDecimal::from_signed_be_bytes_slice_and_exponent(
+            &[
+                1, 53, 169, 169, 173, 175, 83, 216, 15, 110, 137, 47, 175, 202, 192, 196, 222, 179,
+                11, 93, 98, 127, 51, 6, 161, 141, 90, 11, 80, 251, 28,
+            ],
+            69,
+        )),
         "Double" => CqlValue::Double(21.37),
         "Duration" => CqlValue::Duration(CqlDuration {
             months: 21,
