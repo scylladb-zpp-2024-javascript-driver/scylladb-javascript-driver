d1a87c5624939fabecd1464d185a8130c8d86ceb
Stanisław Czech <stasio.czech@gmail.com>
2025-05-14 21:22:53 +0200

    Simplify benchmark initialization

--- benchmark/logic/insert.js
+++ benchmark/logic/insert.js
@@ -2,33 +2,16 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
+const client = new cassandra.Client(utils.getClientArgs());
 const iterCount = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaBasic, next);
         },
         async function insert(next) {
             for (let i = 0; i < iterCount; i++) {
@@ -36,14 +19,14 @@ async.series(
                 const query =
                     "INSERT INTO benchmarks.basic (id, val) VALUES (?, ?)";
                 try {
-                    await client.execute(query, [id, 100], {prepare: true});
+                    await client.execute(query, [id, 100], { prepare: true });
                 } catch (err) {
                     return next(err);
                 }
             }
             next();
         },
-        function r(){
+        function r() {
             exit(0);
         }
     ],
@@ -52,7 +35,7 @@ async.series(
             console.error("There was an error", err.message, err.stack);
             exit(1);
         }
-        
+
     },
 );
 
