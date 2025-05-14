9bb40e36dfef89908fb7b6c1e321a90555907585
Stanisław Czech <stasio.czech@gmail.com>
2025-05-14 19:12:50 +0200

    Introduce repeat capped
    Limit a single execute concurrent t q queries (q = 10^6),
    in order to avoid "JavaScript heap out of memory" problem

--- benchmark/logic/utils.js
+++ benchmark/logic/utils.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const singleStepCount = 1000000;
+
 function getClientArgs() {
     return {
         contactPoints: [process.env.SCYLLA_URI ?? "172.17.0.2:9042"],
@@ -7,4 +9,15 @@ function getClientArgs() {
     };
 }
 
+// call callback, each with up to singleStepCount,
+// multiple times, so that sum of all called callback is equal to n
+async function repeatCapped(callback, n) {
+    for (let rep = 0; rep * singleStepCount < n; rep++) {
+        const finalStep = Math.min(n, (rep + 1) * singleStepCount);
+        await callback(finalStep - rep * singleStepCount);
+    }
+
+}
+
 exports.getClientArgs = getClientArgs;
+exports.repeatCapped = repeatCapped;
