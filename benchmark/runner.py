90549b8434e5319d101a4e16a83bf1f4da2b844b
Stapox35 <stapox35@gmail.com>
2025-05-27 11:41:10 +0200

    Add batch benchmark to runner

--- benchmark/runner.py
+++ benchmark/runner.py
@@ -59,6 +59,7 @@ n_min["concurrent_insert.js"] = 4_000_000 / 64
 n_min["concurrent_select.js"] = 400_000 / 64
 n_min["insert.js"] = 400_000 / 64
 n_min["select.js"] = 100_000 / 64
+n_min["batch.js"] = 3_000_000 / 64
 
 steps = {}
 
@@ -67,13 +68,14 @@ step = 4
 # --------- libs and rust benchmark names ----------
 libs = ["scylladb-javascript-driver", "cassandra-driver"]
 benchmarks = ["concurrent_insert.js", "insert.js", "select.js",
-              "concurrent_select.js"]
+              "concurrent_select.js", "batch.js"]
 
 name_rust = {}
 name_rust["concurrent_insert.js"] = "concurrent_insert_benchmark"
 name_rust["insert.js"] = "insert_benchmark"
 name_rust["select.js"] = "select_benchmark"
 name_rust["concurrent_select.js"] = "concurrent_select_benchmark"
+name_rust["batch.js"] = "batch_benchmark"
 
 
 df = {}
