f894f17c07c0de9e7afa30c2dc22eeb0cd8e8c5a
Stanisław Czech <stanislaw.czech@scylladb.com>
2025-10-02 16:31:54 +0200

    Proper hack

--- .github/test.sh
+++ .github/test.sh
@@ -2,7 +2,8 @@ ccm create test8625732647544266 -v 3.11.4
 ccm populate -n 1:0
 # set -e
 timeout 60 ccm start --wait-for-binary-proto 
-# echo $?
+# echo "TEst"
+echo $?
 # echo "Above is a code from the first run"
 # ccm remove 
 # ccm create test1436203882558078 -v 3.11.4
