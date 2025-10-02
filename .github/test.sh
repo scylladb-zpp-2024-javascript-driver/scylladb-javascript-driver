aadcb9158fe69dfa2f474b5fefe04248c0700fc7
Stanisław Czech <stanislaw.czech@scylladb.com>
2025-10-02 14:02:46 +0200

    More tests

--- .github/test.sh
+++ .github/test.sh
@@ -2,4 +2,11 @@ ccm create test8625732647544266 -v 3.11.4
 ccm populate -n 1:0
 timeout 120 ccm start --wait-for-binary-proto 
 echo $?
+echo "Above is a code from the first run"
+ccm remove 
+ccm create test1436203882558078 -v 3.11.4
+ccm populate -n 3:0
+timeout 120 ccm start --wait-for-binary-proto
+echo $?
+echo "Above is a code from the second run"
 echo "Done"
