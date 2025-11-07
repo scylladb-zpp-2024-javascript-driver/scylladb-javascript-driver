0124ac51df498c749d508f96d669c48b95127823
Stanisław Czech <stasio.czech@gmail.com>
2025-11-07 15:27:59 +0100

    Add unit tests for new JsResult

---
+++ src/tests/js_results_tests.rs
@@ -0,0 +1,16 @@
+use crate::errors::{ConvertedError, JsResult, js_error};
+
+#[napi]
+pub fn tests_return_js_result(kind: i32) -> JsResult<i32> {
+    match kind {
+        1 => JsResult::Ok(1),
+        2 => JsResult::Error(scylla::errors::BadKeyspaceName::Empty.into()),
+        3 => JsResult::NapiError(js_error("Napi-error")),
+        _ => unimplemented!("Unexpected test kind"),
+    }
+}
+
+#[napi]
+pub fn tests_return_converted_error() -> ConvertedError {
+    scylla::errors::BadKeyspaceName::Empty.into()
+}
---
+++ test/unit/js-results-tests.js
@@ -0,0 +1,31 @@
+"use strict";
+const { assert } = require("chai");
+
+const rust = require("../../index");
+const helper = require("../test-helper");
+
+describe("JsResult", function () {
+    it("should get the JsResult::Ok correctly", function () {
+        assert.strictEqual(rust.testsReturnJsResult(1), 1);
+    });
+    it("should get the correct error from JsResult::Error", function () {
+        try {
+            rust.testsReturnJsResult(2);
+            assert.fail("This function should throw");
+        } catch (e) {
+            helper.assertInstanceOf(e, Error);
+            assert.strictEqual(e.message, "Keyspace name is empty");
+            assert.strictEqual(e.name, "BadKeyspaceName");
+        }
+    });
+    it("should get the correct error from JsResult::NapiError", function () {
+        try {
+            rust.testsReturnJsResult(3);
+            assert.fail("This function should throw");
+        } catch (e) {
+            helper.assertInstanceOf(e, Error);
+            assert.strictEqual(e.message, "Error#Napi-error");
+            assert.strictEqual(e.name, "Error");
+        }
+    });
+});
